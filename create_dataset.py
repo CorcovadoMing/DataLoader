205d52617dd570228dbe4656ea3fe480a03d8e1f
Ming <rf37535@gmail.com>
2017-05-26 05:48:35 +0800

    Remove range limitation

--- create_dataset.py
+++ create_dataset.py
@@ -10,7 +10,7 @@ from itertools import count
 def image_loader(path):
     dirlist = [ item for item in os.listdir(path) if os.path.isdir(os.path.join(path, item)) ]
     images = {}
-    for subfolder in dirlist[:5]:
+    for subfolder in dirlist:
         images[subfolder] = []
         imagePath = glob.glob(path + '/' + subfolder +'/*')
         im_array = numpy.array([numpy.array(Image.open(i).convert('RGB'), numpy.uint8) for i in imagePath])
