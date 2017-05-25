7957663dbb34ed7010d68a74c909d1b7525b79b3
Ming <rf37535@gmail.com>
2017-05-26 04:26:24 +0800

    Put the dataset into a dict for further processing

--- create_dataset.py
+++ create_dataset.py
@@ -5,15 +5,15 @@ import glob
 import datum_utils
 import lmdb
 
-def readimage(path):
+def image_loader(path):
     dirlist = [ item for item in os.listdir(path) if os.path.isdir(os.path.join(path, item)) ]
-    images = []
-
+    images = {}
     for subfolder in dirlist:
+        images[subfolder] = []
         imagePath = glob.glob(path + '/' + subfolder +'/*')
         im_array = numpy.array([numpy.array(Image.open(i), 'f') for i in imagePath])
-        images.append(im_array)
+        images[subfolder].append(im_array)
     return images
 
 if __name__ == '__main__':
-    readimage('test_rawdata')
+    print image_loader('test_rawdata').keys()
