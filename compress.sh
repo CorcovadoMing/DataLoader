d52de6374b53b51dfecae16f864b9a76d7bbe1e0
Ming <rf37535@gmail.com>
2017-05-26 12:53:33 +0800

    Add tool to compress the db

--- compress.sh
+++ compress.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+#OPTION=
+INPUT=
+OUTPUT=
+
+while [ "$#" -gt "0" ]
+do
+
+if [ "$1" = "-i" ]; then
+    INPUT=$2
+    shift 2
+
+elif [ "$1" = "-f" ]; then
+    OUTPUT=$2
+    shift 2
+else
+    echo "Usage:"
+    echo "compress.sh [-o OUTPUT] [-i INPUT]"
+fi
+
+done
+
 # Aggresive compression: XZ_OPT=-9 tar Jcvf $1 $2
 # Normal compression: tar czvf $1 $2
+tar cvzf $OUTPUT $INPUT 
