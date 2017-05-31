8a4961026353bf5f51c7be19f1f81b63227d64c0
Ming <rf37535@gmail.com>
2017-05-31 10:22:20 +0800

    Add xz compress option

--- compress.sh
+++ compress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#OPTION=
+XZ=0
 INPUT=
 OUTPUT=
 
@@ -10,10 +10,12 @@ do
 if [ "$1" = "-i" ]; then
     INPUT=$2
     shift 2
-
 elif [ "$1" = "-f" ]; then
     OUTPUT=$2
     shift 2
+elif [ "$1" = "-x" ]; then
+    XZ=1
+    shift 1
 else
     echo "Usage:"
     echo "compress.sh [-o OUTPUT] [-i INPUT]"
@@ -23,4 +25,9 @@ done
 
 # Aggresive compression: XZ_OPT=-9 tar Jcvf $1 $2
 # Normal compression: tar czvf $1 $2
-tar cvzf $OUTPUT $INPUT 
+
+if [ $XZ -eq 1 ]; then
+    XZ_OPT=-9 tar Jcvf $OUTPUT $INPUT
+else
+    tar cvzf $OUTPUT $INPUT 
+fi
