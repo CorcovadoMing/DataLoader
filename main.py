e7c12bd8ba35ded0880023241358e6eb0638dfd8
Ming <rf37535@gmail.com>
2017-06-04 02:57:58 +0800

    Add resize parameter to interface

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='[%Y/%m/%d][%I:%M:%S %p] ', level=logging.INFO)
     parser = argparse.ArgumentParser(description='Data Loader')
     parser.add_argument('--train', help='training dir')
+    parser.add_argument('--resize', help='resize image')
     parser.add_argument('--output', help='output dataset dir')
 
     args = parser.parse_args()
@@ -19,5 +20,5 @@ if __name__ == '__main__':
             db_name = args.train.split('/')[-2]
         if not db_name:
             db_name = args.train
-        images = image_loader(args.train)
+        images = image_loader(args.train, int(args.resize))
         create_lmdb(images, args.output + '/' + db_name)
