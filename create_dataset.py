bd45945f25340d38668bf1b9d43e593202393bc5
CorcovadoMing <rf37535@gmail.com>
2017-07-19 19:54:19 +0800

    Implement spliting from training data

--- create_dataset.py
+++ create_dataset.py
@@ -9,18 +9,28 @@ import pickle
 import numpy as np
 from datum_utils import *
 from PIL import Image
-
+from random import shuffle
 
 def image_loader(path, size=32):
     dirlist = [ item for item in os.listdir(path) if os.path.isdir(os.path.join(path, item)) ]
     logging.info("Create dataset with {0} classes from {1}".format(len(dirlist), path))
     images = {}
     for subfolder in dirlist:
-        images[subfolder] = []
         imagePath = glob.glob(path + '/' + subfolder +'/*')
         im_array = np.array([np.array(Image.open(i).convert('RGB').resize((size, size), Image.ANTIALIAS), np.uint8) for i in imagePath])
-        images[subfolder].append(im_array)
+        images[subfolder] = im_array
     return images
+
+def split_testing(images, ratio=0.20):
+    train_images = {}
+    test_images = {}
+    for key in images:
+        t = len(images[key])
+        sp = int(t * ratio) + 1
+        shuffle(images[key])
+        test_images[key] = images[key][:sp]
+        train_images[key] = images[key][sp:]
+    return train_images, test_images
 
 def write_batch_lmdb(env, batch):
     try:
@@ -43,7 +53,7 @@ def create_lmdb(images, db, batch_size=1024):
     batch = []
     for label in images:
         label_no = l.next()
-        for data in images[label][0]:
+        for data in images[label]:
             datum = array_to_datum(data, label_no)
             batch.append((str(i.next()), datum))
             if len(batch) >= batch_size:
@@ -53,23 +63,29 @@ def create_lmdb(images, db, batch_size=1024):
     batch = []
     env.close()
 
-def create_pickle(images, db):
+def create_pickle(train_images, test_images, db):
     output = db + '.pkl'
     logging.info("Convert dataset into pkl format to {0}".format(output))
-    x = []
-    y = []
-    for label in images:
-        for data in images[label][0]:
-            x.append(data)
+    x, xt = [], []
+    y, yt = [], []
+    for label in train_images:
+        for train in train_images[label]:
+            x.append(train)
             y.append(label)
+        for test in test_images[label]:
+            xt.append(test)
+            yt.append(label)
     x = np.array(x)
     y = np.array(y)
-    pickle.dump((x,y), open(output, 'wb'))
+    xt = np.array(xt)
+    yt = np.array(yt)
+    pickle.dump((x, y, xt, yt), open(output, 'wb'))
 
 
 if __name__ == '__main__':
-    test_dataset = 'caltech101'
+    test_dataset = '/datasets/caltech101'
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='[%Y/%m/%d][%I:%M:%S %p] ', level=logging.INFO)
     images = image_loader(test_dataset)
-    create_lmdb(images, test_dataset)
-    create_pickle(images, test_dataset)
+    train_images, test_images = split_testing(images)
+    create_pickle(train_images, test_images, test_dataset)
+    #create_lmdb(images, test_dataset)
